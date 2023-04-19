ebf49214ff03df0ff551c7a4e39ecf1fd6cc7262
chiho <chihoang1907@gmail.com>
2023-04-19 09:02:15 +0700

    SSD object detection

--- Algorithm/Object_detection/process_data.py
+++ Algorithm/Object_detection/process_data.py
@@ -1,3 +1,16 @@
 import os
 import pandas as pd
 
+directory = "dataset/"
+dir_inputs = directory + "JPEGImages/"
+dir_outputs = directory + "Annotations/"
+
+file_data = "data/data.csv"
+
+for root, dirs, files in os.walk(dir_inputs):
+    inputs = [os.path.join(root, f) for f in files]
+
+for root, dirs, files in os.walk(dir_outputs):
+    outputs = [os.path.join(root, f) for f in files]
+
+pd.DataFrame({"Input":inputs,"Output":outputs}).to_csv(file_data, index= False)
